8d7d2957bcbba80c4c63fb2c3d50d7abab0928e4
christianlandbo <cla@nineconsult.dk>
2012-12-23 20:31:21 +0100

    Updated to Grails 2.1.1 Updated plugins to latest versions

--- whalewired/grails-app/jobs/com.whalewired.jobs/DeleteOldLogsJob.groovy
+++ whalewired/grails-app/jobs/com.whalewired.jobs/DeleteOldLogsJob.groovy
@@ -1,11 +1,13 @@
 package com.whalewired.jobs
 
+import org.quartz.CronTrigger;
+
 import com.whalewired.services.*
 
 class DeleteOldLogsJob {
 	def concurrent = false
 	static triggers = {
-		cron name: 'deleteOldLogs', cronExpression: '0 0 12 * * ?'
+		cron name: 'deleteOldLogs', cronExpression: '0 0 12 * * ?'   
 		//cron name: 'deleteOldLogs', cronExpression: '0 * * * * ?'
 	}
 		
@@ -16,7 +18,12 @@ class DeleteOldLogsJob {
 		
 		def indices = elasticSearchAdminService.getIndices();
 		for (index in indices) {
-			elasticSearchAdminService.deleteLogs(index.key, index.value.getSettings().get("index.ttl").toInteger());
+			def ttl = index.value.getSettings().get("index.ttl")?.toInteger()
+			if (ttl) {
+				elasticSearchAdminService.deleteLogs(index.key, ttl);
+			} else {
+			println  "[${index.key}] has no TTL definition"
+			}
 		}
 	}
 			
